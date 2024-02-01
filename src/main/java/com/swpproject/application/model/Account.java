374963310e777bca52a6e2a185854dab3a5943b3
tctqn <trinhtruongcong3@gmail.com>
2024-02-01 16:58:11 +0700

    create models for authentication feature

---
+++ src/main/java/com/swpproject/application/model/Account.java
@@ -0,0 +1,12 @@
+package com.swpproject.application.model;
+
+public class Account {
+    private Integer Id;
+    private String fullName;
+    private String email;
+    private String password;
+    private Character gender;
+    private String phone;
+    
+
+}

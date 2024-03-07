3c2a0758d7befc95b4d5e058534c1a64b83d9dc3
Thành Đạt Lưu <luuthanhdat19022021@gmail.com>
2024-03-07 23:51:12 +0700

    submit check create, update for exercise, nutrition

--- src/main/resources/META-INF/resources/assets/js/exercise/create/exercise-create-submit-handler.js
+++ src/main/resources/META-INF/resources/assets/js/exercise/create/exercise-create-submit-handler.js
@@ -1,96 +1,77 @@
+function clearError(field) {
+    $(field).text('');
+}
+
+function showError(field, message) {
+    $(field).text(message);
+}
+
+function validateSelect(select, errorId, errorMessage) {
+    var value = $(select).val();
+    if (value === 'None') {
+        showError(errorId, errorMessage);
+        return false;
+    }
+
+    clearError(errorId);
+    return true;
+}
+
 $(document).ready(function() {
     $('#exerciseForm').submit(function(event) {
+
         var errors = false;
 
-        function showError(field, message) {
-            $(field).text(message);
+        clearError('#exerciseNameError');
+        clearError('#exerciseDescriptionError');
+        clearError('#levelError');
+        clearError('#muscleError');
+        clearError('#imageError');
+        clearError('#youtubeLinkError');
+
+        var exerciseName = $('#exerciseName').val();
+        if (exerciseName.length < 5) {
+            showError('#exerciseNameError', 'Exercise Name must be at least 5 characters long');
             errors = true;
         }
 
-        function clearError(field) {
-            $(field).text('');
-        }
-
-        function validateRequiredInput(input, errorMessage) {
-            var value = $(input).val().trim();
-            if (value === '') {
-                showError(errorMessage);
-            } else {
-                clearError(errorMessage);
-            }
-            return value;
-        }
-
-        function validateSelect(select, errorId, errorMessage) {
-            var value = $(select).val();
-            if (value === 'None') {
-                showError(errorId, errorMessage);
-            } else {
-                clearError(errorId);
-            }
-            return value;
-        }
-
-        function validateImageSize(input, errorMessage) {
-            var file = $(input)[0].files[0];
-            if (file === undefined) {
-                showError(errorMessage);
-            } else {
-                if (file.size > 10 * 1024 * 1024) {
-                    showError(errorMessage);
-                } else {
-                    clearError(errorMessage);
-                }
-            }
-        }
-
-        function validateYouTubeLink(input, regex, errorMessage) {
-            var link = $(input).val().trim();
-            if (link === '') {
-                showError(errorMessage);
-            } else {
-                if (!regex.test(link)) {
-                    showError(errorMessage);
-                } else {
-                    clearError(errorMessage);
-                }
-            }
-        }
-
-        clearError('#exerciseNameError'); // Clear error message before validation
-        var exerciseName = validateRequiredInput('input[name="exerciseName"]', '#exerciseNameError');
-        if (exerciseName.length < 5) {
-            showError('#exerciseNameError', 'Exercise Name must be at least 5 characters long');
-        }
-
-        clearError('#levelError'); // Clear error message before validation
-        if (!$('input[name="levelRadio"]:checked').val()) {
-            showError('#levelError', 'Please select a level');
-        }
-
-        clearError('#equipmentError'); // Clear error message before validation
-        validateSelect('select[name="equipment"]', '#equipmentError', 'Please select a equipment');
-
-        clearError('#muscleError'); // Clear error message before validation
-        validateSelect('select[name="muscle"]', '#muscleError', 'Please select a affected muscle');
-
-        clearError('#imageError'); // Clear error message before validation
-        validateImageSize('input[name="image"]', '#imageError');
-        if (!$('#imageInput').val()) { // If no image is selected in the file input
-            $('#imageError').text('No image selected. Please select an image.');
-        }
-
-
-        clearError('#youtubeLinkError'); // Clear error message before validation
-        validateYouTubeLink('#youtubeLink', /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=)([a-zA-Z0-9_-]{11})$/, '#youtubeLinkError');
-
-        clearError('#exerciseDescriptionError'); // Clear error message before validation
-        var exerciseDescription = validateRequiredInput('#exerciseDescription', '#exerciseDescriptionError');
+        var exerciseDescription = $('#exerciseDescription').val();
         if (exerciseDescription.length < 10) {
             showError('#exerciseDescriptionError', 'Exercise Description must be at least 10 characters long');
+            errors = true;
         }
 
-        if (errors) {
+        if (!$('input[name="levelRadio"]:checked').val()) {
+            showError('#levelError', 'Please select a level');
+            errors = true;
+        }
+
+        var validMuscle = validateSelect('select[name="muscle"]', '#muscleError', 'Please select an affected muscle');
+        if (validMuscle === false) {
+            errors = true;
+        }
+
+        var file = $('input[name="image"]')[0].files[0];
+        if (file === undefined) {
+            showError('#imageError', 'No image selected. Please select an image.');
+            errors = true;
+        } else {
+            if (file.size > 10 * 1024 * 1024) {
+                showError('#imageError', 'Image selected should be lower than 10MB');
+                errors = true;
+            }else{
+                clearError('#imageError');
+            }
+        }
+
+        var regex = /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=)([a-zA-Z0-9_-]{11})$/;
+        var link = $('#youtubeLink').val().trim();
+        if (!regex.test(link)) {
+            showError('#youtubeLinkError', 'Invalid YouTube link format');
+            errors = true;
+        }
+
+        if(errors === true){
             event.preventDefault();
         }
     });

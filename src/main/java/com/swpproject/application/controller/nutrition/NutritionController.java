41746e72d5eb2cc30f2a221e145a208a77ef6bfb
Thành Đạt Lưu <luuthanhdat19022021@gmail.com>
2024-03-07 10:58:28 +0700

    refactor nutrition controller to nutrition service

--- src/main/java/com/swpproject/application/controller/nutrition/NutritionController.java
+++ src/main/java/com/swpproject/application/controller/nutrition/NutritionController.java
@@ -1,16 +1,13 @@
 package com.swpproject.application.controller.nutrition;
 
 import com.swpproject.application.model.Nutrition;
-import com.swpproject.application.model.PersonalTrainer;
-import com.swpproject.application.repository.PersonalTrainerRepository;
+import com.swpproject.application.model.NutritionDTOIn;
+import com.swpproject.application.model.NutritionDTOOut;
+import com.swpproject.application.service.impl.NutritionServiceIml;
 import com.swpproject.application.utils.JsonUtils;
-import com.swpproject.application.repository.NutritionRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -18,55 +15,24 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/nutrition")
 public class NutritionController {
-    private final PersonalTrainerRepository personalTrainerRepository;
-    private final NutritionRepository nutritionRepository;
 
-    public NutritionController(PersonalTrainerRepository personalTrainerRepository, NutritionRepository nutritionRepository) {
-        this.personalTrainerRepository = personalTrainerRepository;
-        this.nutritionRepository = nutritionRepository;
-    }
+    @Autowired
+    private NutritionServiceIml nutritionServiceIml;
 
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = "text/html; charset=UTF-8")
     public String getExerciseListPage(ModelMap model) {
-        List<Nutrition> list = nutritionRepository.findAll();
-        List<NutritionDTOOut> listOut = getNutritionDTOOutListFromNutritionList(list);
-
-        String json = JsonUtils.jsonConvert(listOut);
-        model.addAttribute("nutritionList", json);
+        List<NutritionDTOOut> nutritionDTOOutList = nutritionServiceIml.getNutritionDTOOutList();
+        String nutritionDTOOutListJson = JsonUtils.jsonConvert(nutritionDTOOutList);
+        model.addAttribute("nutritionList", nutritionDTOOutListJson);
         return "nutrition-list";
     }
-
-    private List<NutritionDTOOut> getNutritionDTOOutListFromNutritionList(List<Nutrition> nutritionsList) {
-        return nutritionsList.stream()
-                .map(this::getNutritionDTOOutbyNutrition)
-                .collect(Collectors.toList());
-    }
-
-    private NutritionDTOOut getNutritionDTOOutbyNutrition(Nutrition nutrition){
-        NutritionDTOOut nutritionDTOOut = new NutritionDTOOut();
-        nutritionDTOOut.setNutritionId(nutrition.getNutritionId());
-        nutritionDTOOut.setName(nutrition.getName());
-        nutritionDTOOut.setCaloIn(nutrition.getCaloIn());
-        nutritionDTOOut.setProtein(nutrition.getProtein());
-        nutritionDTOOut.setFat(nutrition.getFat());
-        nutritionDTOOut.setCarb(nutrition.getCarb());
-        nutritionDTOOut.setImageDescription(nutrition.getImageDescription());
-        nutritionDTOOut.setIsPrivate(nutrition.getIsPrivate() + "");
-        nutritionDTOOut.setPersonalTrainerId(nutrition.getPersonalTrainer().getId());
-        nutritionDTOOut.setPersonalTrainerImage(nutrition.getPersonalTrainer().getAccount().getAvatarImage());
-        return nutritionDTOOut;
-    }
-
 
     //Get view create nutrition
     @RequestMapping(value = "/create", method = RequestMethod.GET, produces = "text/html; charset=UTF-8")
@@ -75,23 +41,8 @@ public class NutritionController {
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST, produces = "text/html; charset=UTF-8")
-    public String createExercise(@ModelAttribute NutritionDTOIn nutritionDTO, Model model) throws IOException {
-        Nutrition nutrition = new Nutrition();
-        nutrition.setName(nutritionDTO.getNutritionName());
-        nutrition.setCaloIn(nutritionDTO.getCalories());
-        nutrition.setProtein(nutritionDTO.getProtein());
-        nutrition.setFat(nutritionDTO.getFat());
-        nutrition.setCarb(nutritionDTO.getCarb());
-        nutrition.setImageDescription(nutritionDTO.getImage().getBytes());
-
-        String isPrivateString = nutritionDTO.getIsPrivate();
-        int isPrivateBoolean = isPrivateString == null ? 0 : 1;
-        nutrition.setIsPrivate(isPrivateBoolean);
-
-        PersonalTrainer personalTrainerExample = personalTrainerRepository.findAll().getFirst();
-        nutrition.setPersonalTrainer(personalTrainerExample);
-
-        nutritionRepository.save(nutrition);
+    public String createExercise(@ModelAttribute NutritionDTOIn nutritionDTOIn, Model model) throws IOException {
+        nutritionServiceIml.create(nutritionDTOIn);
         return "redirect:/nutrition/";
     }
 
@@ -101,8 +52,8 @@ public class NutritionController {
         HttpSession session = request.getSession();
         session.setAttribute("nutritionId", id);
 
-        Nutrition nutrition = nutritionRepository.findById(id).get();
-        NutritionDTOOut nutritionDTOOut = getNutritionDTOOutbyNutrition(nutrition);
+        Nutrition nutrition = nutritionServiceIml.findNutritionById(id).get();
+        NutritionDTOOut nutritionDTOOut = nutrition.getNutritionDTOOut();
 
         String json = JsonUtils.jsonConvert(nutritionDTOOut);
         model.addAttribute("nutrition", json);
@@ -116,16 +67,7 @@ public class NutritionController {
         Integer id = (Integer) session.getAttribute("nutritionId");
 
         if (id != null) {
-            Nutrition nutrition = nutritionRepository.findById(id).get();
-            nutrition.setName(nutritionDTOIn.getNutritionName());
-            nutrition.setCaloIn(nutritionDTOIn.getCalories());
-            nutrition.setProtein(nutritionDTOIn.getProtein());
-            nutrition.setFat(nutritionDTOIn.getFat());
-            nutrition.setCarb(nutritionDTOIn.getCarb());
-            System.out.println(nutrition.getPersonalTrainer().getId());
-            if(nutritionDTOIn.getImage().getBytes().length != 0) nutrition.setImageDescription(nutritionDTOIn.getImage().getBytes());
-            nutritionRepository.save(nutrition);
-
+            nutritionServiceIml.update(nutritionDTOIn, id);
             session.removeAttribute("exerciseId");
             return "redirect:/nutrition/";
         } else {
@@ -134,34 +76,6 @@ public class NutritionController {
     }
 }
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-class NutritionDTOIn {
-    private String nutritionName;
-    private Float calories;
-    private Float protein;
-    private Float fat;
-    private Float carb;
-    private MultipartFile image;
-    private String isPrivate;
-}
 
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-class NutritionDTOOut {
-    private int nutritionId;
-    private String name;
-    private Float caloIn;
-    private Float protein;
-    private Float fat;
-    private Float carb;
-    private byte[] imageDescription;
-    private String isPrivate;
-    private int personalTrainerId;
-    private byte[] personalTrainerImage;
-}
+
---
+++ src/main/java/com/swpproject/application/controller/nutrition/NutritionSearchController.java
@@ -0,0 +1,9 @@
+package com.swpproject.application.controller.nutrition;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value ="/api/nutritions")
+public class NutritionSearchController {
+}
